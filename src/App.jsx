2bb737f9f4957c37c400fd49e7db1aab68e68812
SaarthakSinghal <saarthaksinghal0607@gmail.com>
2025-05-31 22:07:12 +0530

    1.2 Added some initial background styling and written a rough outline of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='px-4 md:px-8 lg:pd-16 2xl:px-64'>
+      {/* NAVBAR */}
+      {/* BREADCRUMBS */}
+      {/* INTRODUCTION */}
+      {/* FEATURED POSTS */}
+      {/* POST LIST */}
+    </div>
   )
 }
 
