af9ad6899fe301a78146068059db26ca73c8b932
SaarthakSinghal <saarthaksinghal0607@gmail.com>
2025-06-02 01:25:25 +0530

    1.2.1 Implement Navbar component with initial layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
     <div className='px-4 md:px-8 lg:pd-16 2xl:px-64'>
-      {/* NAVBAR */}
+      <Navbar />
       {/* BREADCRUMBS */}
       {/* INTRODUCTION */}
       {/* FEATURED POSTS */}
-      {/* POST LIST */}   
+      {/* POST LIST */}
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 const Navbar = () => {
   return (
-    <div className=''>
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
+      <div className="flex items-center gap-4 text-lg md:text-xl font-semibold">
+        <img src="/logo.png" alt="logo" className="w-8 h-8" />
+        <span>lamalog</span>
+      </div>
       {/* MOBILE MENU */}
+      <div className="md:hidden"></div>
       {/* DESKTOP MENU */}
+      <div className="hidden md:flex"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
